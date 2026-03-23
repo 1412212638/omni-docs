5017eab8070203a5a2548a158d39001994dfd100
Zakkkk <1412212638@qq.com>
2026-03-24 05:06:51 +0800

    Simplify embedded API page layout

--- docs/.vitepress/theme/components/ApiReference.vue
+++ docs/.vitepress/theme/components/ApiReference.vue
@@ -1,32 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
 const props = withDefaults(defineProps<{ embedUrl?: string }>(), {
   embedUrl: 'https://omnirouters.apifox.cn/'
 })
-
-const isLoaded = ref(false)
 </script>
 
 <template>
   <div class="embed-shell">
-    <div class="embed-frame-wrap">
-      <div class="embed-actions">
-        <a class="embed-link" :href="props.embedUrl" target="_blank" rel="noreferrer">
-          新窗口打开
-        </a>
-      </div>
-      <div v-if="!isLoaded" class="embed-loading">正在加载 Apifox 文档...</div>
-      <iframe
-        class="embed-frame"
-        :class="{ 'is-visible': isLoaded }"
-        :src="props.embedUrl"
-        title="OmniRouters Apifox Documentation"
-        loading="eager"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allow="clipboard-read; clipboard-write"
-        @load="isLoaded = true"
-      />
-    </div>
+    <iframe
+      class="embed-frame"
+      :src="props.embedUrl"
+      title="OmniRouters Apifox Documentation"
+      loading="eager"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allow="clipboard-read; clipboard-write"
+    />
   </div>
 </template>
